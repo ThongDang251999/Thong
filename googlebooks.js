b2b450339cc6d0e11d1c1c42645da7bd4c20c1f9
ThongDang251999 <80073428+ThongDang251999@users.noreply.github.com>
2025-02-02 23:57:26 -0700

    Add files via upload

---
+++ googlebooks.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+
+module.exports = async (phrase) => {
+    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+        params: {
+            format: 'json',
+            q: `${phrase}`
+        }
+    })
+
+
+    //
+    //TODO: return a formattedObj
+    var formattedObj = {
+        data: response.data, 
+        status: response.status, 
+        statusText: response.statusText, 
+        headers: response.headers,
+        requestHeader: response.config.headers
+    }
+
+    //TODO: return the formattedObject
+    return JSON.stringify(results.data);
+}
